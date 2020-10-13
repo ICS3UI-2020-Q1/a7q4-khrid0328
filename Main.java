ce2b28281fa004f56eabebb36474c330e3d92a7e
khrid0328 <khrid0328@wrdsb.ca>
2020-10-13 15:47:24 +0000

    Added comments

--- Main.java
+++ Main.java
@@ -4,8 +4,13 @@
  */
 public class Main {
 
+// creating a methos that returns the last digit of an integer
 public static int lastDigit(int integer){
+
+  // the last integer of the number is the remainder when dividing by 10
   int lastint = integer % 10;
+
+  // return the remainder which is the last digit of the integer
   return lastint;
 }
 
@@ -15,7 +20,10 @@ public static int lastDigit(int integer){
    */
   public static void main(String[] args) {
     
+    // determine the number that you want to know its last integer. works with user's input as well.
     int digit = lastDigit(1467);
+
+    // print the last digit to the screen
     System.out.println(digit);
   }
 }
