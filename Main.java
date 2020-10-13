e7d7eb9db7f50c84f5bdbdd6d556ff5a9141c9ea
khrid0328 <khrid0328@wrdsb.ca>
2020-10-13 15:09:04 +0000

    A7Q4 this program creates a method named lastDigit that returns the last digit of an integer.

--- Main.java
+++ Main.java
@@ -1,15 +1,21 @@
 /**
- *
- * @author 
+ * this program creates a method named lastDigit that returns the last digit of an integer.
+ * @author Dafna Khripun
  */
 public class Main {
+
+public static int lastDigit(int integer){
+  int lastint = integer % 10;
+  return lastint;
+}
 
   /**
    * The method that is executed when you hit the run button.
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
     
+    int digit = lastDigit(1467);
+    System.out.println(digit);
   }
 }
